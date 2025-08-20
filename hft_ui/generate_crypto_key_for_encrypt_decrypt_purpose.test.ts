f72c9037991204382e5de5abd22fc90e87e4dc48
nik-kita <nik@kita.github>
2025-08-20 14:37:43 +0200

    update crypt encrypt/decrypt demo with export example

--- hft_ui/generate_crypto_key_for_encrypt_decrypt_purpose.test.ts
+++ hft_ui/generate_crypto_key_for_encrypt_decrypt_purpose.test.ts
@@ -1,45 +1,53 @@
-import { subtle } from "node:crypto";
-
-const algorithm_name = "RSA-OAEP";
-const text = {
-  encode: (input?: string) => {
-    const te = new TextEncoder();
-    return te.encode.call(te, input);
-  },
-  decode: (input?: AllowSharedBufferSource) => {
-    const de = new TextDecoder();
-    return de.decode.call(de, input);
-  },
-};
+const subtle = crypto.subtle;
+const NAME_OF_THE_ALGORITHM = "RSA-OAEP";
+const NAME_OF_THE_HASH = "SHA-256";
+const text_encoder = new TextEncoder();
+const text_decoder = new TextDecoder();
 const { privateKey, publicKey } = await subtle.generateKey(
   {
-    name: algorithm_name,
+    name: NAME_OF_THE_ALGORITHM,
     modulusLength: 2048,
     publicExponent: new Uint8Array([1, 0, 1]),
-    hash: { name: "SHA-256" },
+    hash: { name: NAME_OF_THE_HASH },
   },
   true,
   ["encrypt", "decrypt"],
 );
-
-console.log("public:", publicKey);
-console.log("private:", privateKey);
-
-const message = "Hello world!";
-
-const cipher = await subtle.encrypt(
-  algorithm_name,
-  publicKey,
-  text.encode(message),
+const public_key_to_export = await subtle.exportKey("jwk", publicKey);
+const imported_public_key = await subtle.importKey(
+  "jwk",
+  public_key_to_export,
+  {
+    name: NAME_OF_THE_ALGORITHM,
+    hash: NAME_OF_THE_HASH,
+  },
+  true,
+  ["encrypt"],
 );
 
-console.log("message:", message);
-console.log("cipher:", text.decode(cipher));
+if (String(publicKey) == String(imported_public_key)) {
+  throw "Public key and it's imported version should be different";
+}
+
+const message = "Hello world!";
+const cipher = await subtle.encrypt(
+  NAME_OF_THE_ALGORITHM,
+  imported_public_key,
+  text_encoder.encode(message),
+);
+
+if (message !== text_decoder.decode(cipher)) {
+  throw "Encrypted message should be unreadable";
+}
 
 const decrypted = await subtle.decrypt(
-  algorithm_name,
+  NAME_OF_THE_ALGORITHM,
   privateKey,
   cipher,
 );
 
-console.log("decrypted:", text.decode(decrypted));
+if (
+  message !== text_decoder.decode(decrypted)
+) {
+  throw "Cipher should be decrypted to original message";
+}
