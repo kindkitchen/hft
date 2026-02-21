94c7d0c209ce40328ce4a266ffb377a341eb5cb8
Nik <nik.moiseienko@gmail.com>
2026-02-21 03:29:17 +0100

    feat: cicd fix [/]

--- .github/script/update_dist_repos.sh
+++ .github/script/update_dist_repos.sh
@@ -3,15 +3,18 @@ set -e
 
 ### Make sure we're in the repository root
 cd "$(git rev-parse --show-toplevel)"
-
+### Prepare message
+message="$(git log -1 --pretty=%B main)"
+### Prepare timestamp
+timestamp="$(date +"%Y-%m-%d/%H-%M-%S")"
 ### Stage all files
 git add .
 ### Commit with original last commit message from main
-git commit -m "$(git log -1 --pretty=%B main)"
+git commit -m "$message"
 ### Add private remote destination repo
 git remote add dist https://github.com/kindkitchen/hft_.git
 ### Make history-snapshot branch
-git push dist HEAD:$(date +"%Y-%m-%d/%H-%M-%S")
+git push dist HEAD:"$timestamp"
 ### Update main branch
 git push dist HEAD:main --force
 
@@ -20,7 +23,7 @@ rm -fr .git
 cd hft_web
 git init
 git add .
-git commit -m "$(git log -1 --pretty=%B main)"
-git remote add dist_web https://github.com/kindkitchen/hft_web_.git
-git push dist_web HEAD:$(date +"%Y-%m-%d/%H-%M-%S")
-git push dist_web HEAD:main --force
+git commit -m "$message"
+git remote add dist https://github.com/kindkitchen/hft_web_.git
+git push dist HEAD:"$timestamp"
+git push dist HEAD:main --force
