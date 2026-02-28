855d9f91c2407d8eb5180d1f983d7dbad032c7d4
Nik <nik.moiseienko@gmail.com>
2026-02-28 02:50:21 +0100

    fix: cicd [/]

--- .github/script/update_dist_repos.sh
+++ .github/script/update_dist_repos.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Ensure we are in repository root"
+just printInfo "Ensure we are in repository root"
 cd "$(git rev-parse --show-toplevel)"
 
-echo "Prepare last commit message"
+just printInfo "Prepare last commit message"
 message="$(git log -1 --pretty=%B main)"
 
-echo "Prepare timestamp"
+just printInfo "Prepare timestamp"
 timestamp="$(date +"%Y-%m-%d/%H-%M-%S")"
 
-echo "Array of pairs: 'PATH_TO_REPO|remote_repo_url'"
+just printInfo "Array of pairs: 'PATH_TO_REPO|remote_repo_url'"
 REPOS=(
   "./|https://github.com/kindkitchen/hft_.git"
   "./hft_web|https://github.com/kindkitchen/hft_web_.git"
@@ -19,17 +19,17 @@ REPOS=(
 for entry in "${REPOS[@]}"; do
   IFS="|" read -r PATH_TO_REPO REMOTE <<< "$entry"
 
-  echo "Processing $PATH_TO_REPO -> $REMOTE"
+  just printInfo "Processing $PATH_TO_REPO -> $REMOTE"
 
   if [[ "$PATH_TO_REPO" == "./" ]]; then
-    echo "At root repo, normal push"
+    just printInfo "At root repo, normal push"
 
     git remote remove origin 2>/dev/null || true
     git remote add origin "$REMOTE"
     git add -A
 
     if git diff --cached --quiet; then
-      echo "⚠️ No changes in root repo, skipping..."
+      just printInfo "⚠️ No changes in root repo, skipping..."
       continue
     fi
 
@@ -37,10 +37,13 @@ for entry in "${REPOS[@]}"; do
     git branch -M main
     git push origin main --force
   else
-    echo "Non-root path, copying .git folder"
+    just printInfo "Non-root path, copying .git folder"
     cp -r .git "$PATH_TO_REPO/.git"
 
     cd "$PATH_TO_REPO"
+
+    git remote remove origin 2>/dev/null || true
+    git remote add origin "$REMOTE"
 
     # create orphan branch
     git checkout --orphan "$timestamp"
@@ -52,11 +55,11 @@ for entry in "${REPOS[@]}"; do
     git push origin HEAD:"$timestamp" --force
     git push origin HEAD:main --force
 
-    echo "Cleaning up copied .git"
+    just printInfo "Cleaning up copied .git"
     rm -rf .git
 
     cd - >/dev/null
   fi
 
-  echo "---------------------------"
+  just printInfo "---------------------------"
 done
